055ba3bee752230c90d8f51131bd554377eeeddf
andreucm <andreu@beta-robots.com>
2017-11-17 02:01:39 +0100

    Updated with camera info subscriber and callback implemented.

--- src/ros_img_processor_node.cpp
+++ src/ros_img_processor_node.cpp
@@ -1,23 +1,18 @@
 #include "ros_img_processor_node.h"
 
-RosImgProcessorNode::RosImgProcessorNode() : 
+RosImgProcessorNode::RosImgProcessorNode() :
     nh_(ros::this_node::getName()),
     img_tp_(nh_)
 {
-    //loop rate [hz], Could be set from a yaml file 
-    rate_=10; 
-    
-    //sets publishers
-    image_pub_ = img_tp_.advertise("image_out", 100);
-    
-    //sets subscribers
-    image_subs_ = img_tp_.subscribe("image_in", 1, &RosImgProcessorNode::imageCallback, this);
-    camera_info_subs_ = nh_.subscribe("camera_info_in", 100, &RosImgProcessorNode::cameraInfoCallback, this);
-    
-    //getting package path
-     std::string pkg_path;
-     pkg_path = ros::package::getPath("ros_img_processor");
-     std::cout << "------------------------> " << pkg_path << std::endl;
+	//loop rate [hz], Could be set from a yaml file
+	rate_=10;
+
+	//sets publishers
+	image_pub_ = img_tp_.advertise("image_out", 100);
+
+	//sets subscribers
+	image_subs_ = img_tp_.subscribe("image_in", 1, &RosImgProcessorNode::imageCallback, this);
+	camera_info_subs_ = nh_.subscribe("camera_info_in", 100, &RosImgProcessorNode::cameraInfoCallback, this);
 }
 
 RosImgProcessorNode::~RosImgProcessorNode()
@@ -28,26 +23,27 @@ RosImgProcessorNode::~RosImgProcessorNode()
 void RosImgProcessorNode::process()
 {
     cv::Rect_<int> box;
-    
+
     //check if new image is there
     if ( cv_img_ptr_in_ != nullptr )
     {
         //copy the input image to the out one
         cv_img_out_.image = cv_img_ptr_in_->image;
-        
-        //sets the bounding box of the detection jj
+
+		//find the ball
+		//TODO
+
+		//find the direction vector
+		//TODO
+
+        //sets and draw a bounding box around the ball
         box.x = (cv_img_ptr_in_->image.cols/2)-10;
         box.y = (cv_img_ptr_in_->image.rows/2)-10;
         box.width = 20;
         box.height = 20;
-
-        //mark a rectangle in the center: http://docs.opencv.org/2.4.11/modules/core/doc/drawing_functions.html#rectangle
         cv::rectangle(cv_img_out_.image, box, cv::Scalar(0,255,255), 3);
-        
-        //Mark an ellipse: http://docs.opencv.org/2.4.11/modules/core/doc/drawing_functions.html#ellipse
-        cv::ellipse(cv_img_out_.image,cv::Point(50,50),cv::Size(20,10),27,0,360,cv::Scalar(0,0,255),2);
     }
-    
+
     //reset input image
     cv_img_ptr_in_ = nullptr;
 }
@@ -55,11 +51,14 @@ void RosImgProcessorNode::process()
 void RosImgProcessorNode::publish()
 {
     //image_raw topic
-    cv_img_out_.header.seq ++;
-    cv_img_out_.header.stamp = ros::Time::now();
-    cv_img_out_.header.frame_id = "camera"; 
-    cv_img_out_.encoding = img_encoding_;
-    image_pub_.publish(cv_img_out_.toImageMsg());
+	if(cv_img_out_.image.data)
+	{
+	    cv_img_out_.header.seq ++;
+	    cv_img_out_.header.stamp = ros::Time::now();
+	    cv_img_out_.header.frame_id = "camera";
+	    cv_img_out_.encoding = img_encoding_;
+	    image_pub_.publish(cv_img_out_.toImageMsg());
+	}
 }
 
 double RosImgProcessorNode::getRate() const
@@ -78,11 +77,13 @@ void RosImgProcessorNode::imageCallback(const sensor_msgs::ImageConstPtr& _msg)
     {
         ROS_ERROR("RosImgProcessorNode::image_callback(): cv_bridge exception: %s", e.what());
         return;
-    }      
+    }
 }
 
 void RosImgProcessorNode::cameraInfoCallback(const sensor_msgs::CameraInfo & _msg)
 {
-    //
+	matrixP_ = (cv::Mat_<double>(3,3) << _msg.P[0],_msg.P[1],_msg.P[2],
+                                        _msg.P[3],_msg.P[4],_msg.P[5],
+                                        _msg.P[6],_msg.P[7],_msg.P[8]);
+	//std::cout << matrixP_ << std::endl;
 }
-
